7fbe065df967fbeb822cb6d40262c82ce9f456fc
Nguyễn Bá Thái <thai.nguyen@aegona.com>
2022-09-12 09:44:59 +0700

    change account

--- src/models/UserClient.py
+++ src/models/UserClient.py
@@ -5,3 +5,5 @@ class UserClient(models.Model):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255, null=True)
     name = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
